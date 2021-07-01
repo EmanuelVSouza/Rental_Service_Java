65a62c2bf4dec7e6cef35d8bd0d7dcc207243344
EmanuelVSouza <emanuel_vixboy@hotmail.com>
2021-07-01 00:07:32 -0300

    Projeto final sem interfaces

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -1,21 +1,37 @@
 package model.services;
 
 import model.entities.CarRental;
+import model.entities.Invoice;
 
 public class RentalService {
 
 	private Double pricePerDay;
-	private Double privePerHour;
+	private Double pricePerHour;
 
-	private BrazilTaxService brazilTaxService;
+	private BrazilTaxService taxService;;
 
-	public RentalService(Double pricePerDay, Double privePerHour, BrazilTaxService brazilTaxService) {
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService brazilTaxService) {
 		this.pricePerDay = pricePerDay;
-		this.privePerHour = privePerHour;
-		this.brazilTaxService = brazilTaxService;
+		this.pricePerHour = pricePerHour;
+		this.taxService = brazilTaxService;
 	}
 
 	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
+		double basicPyament;
+
+		if (hours <= 12.0) {
+			basicPyament = Math.ceil(hours) * pricePerHour;
+		} 
+		else {
+			basicPyament = Math.ceil(hours / 24) * pricePerDay;
+		}
+
+		double tax = taxService.tax(basicPyament);
+		
+		carRental.setInvoice(new Invoice(basicPyament, tax));
 
 	}
 
